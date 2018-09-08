2a0044488679c35c4a929a5326efd6cd3d13c1dd
Grzegorz Dybala <gregdybala@gmail.com>
2018-09-08 12:07:00 +0200

    Czwarty commit

---
+++ src/main/java/com/sda/java/basic/CzwartaKlasa.java
@@ -0,0 +1,17 @@
+package com.sda.java.basic;
+
+public class CzwartaKlasa {
+//    dmas
+//    dsafas
+//            dsfa
+//    dsaf
+//                    sdf
+//    dsfa
+//
+//    public dsfa getDsaf() {
+//        dsfd
+//                fsdf
+//                dsfa
+//
+//    }
+}
