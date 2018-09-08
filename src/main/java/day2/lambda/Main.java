ca1e8c463093052ad1e0c782e89ad4b29bdf0c3c
Grzegorz Dybala <gregdybala@gmail.com>
2018-09-08 13:14:37 +0200

    Piaty commit

---
+++ src/main/java/day2/lambda/Main.java
@@ -0,0 +1,22 @@
+package day2.lambda;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void sample() {
+
+        Func add = new Func() {
+            @Override
+            public int calc(int a, int b) {
+                return a + b;
+
+            }
+        };
+        int c1 = 7;
+        int c2 = 3;
+//        System.out.println(add(c1, c2) + " ");
+    }
+}
