a28374405a4d50c02f6d0ce4420b474a2bc44d63
Grzegorz Dybala <gregdybala@gmail.com>
2018-09-09 13:28:35 +0200

    Ósmy commit

---
+++ src/main/java/day3/generic/Person.java
@@ -0,0 +1,66 @@
+package day3.generic;
+
+public abstract class Person {
+    String firstName;
+    String lastName;
+    int age;
+    Status status;
+
+    public Person() {
+
+    }
+
+    public Person(Status status) {
+        this.status = status;
+    }
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", status=" + status +
+                '}';
+    }
+
+    
+}
---
+++ src/main/java/day3/generic/Teacher.java
@@ -0,0 +1,4 @@
+package day3.generic;
+
+public class Teacher {
+}
